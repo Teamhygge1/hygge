50c178285762f8fbb5f0b4e0d7f40a9b576c2c42
Luisa <44492107+Teamhygge1@users.noreply.github.com>
2018-11-16 08:33:40 +0100

    Add files via upload

---
+++ Webprojekt/followbutton.php
@@ -0,0 +1,18 @@
+<?php ?>
+<html>
+<head>
+    <title>hygge</title>
+</head>
+
+
+<body>
+
+
+<button type="button" class="btn btn-follow" >follow</button>
+
+
+
+</body>
+</html>
+
+
---
+++ Webprojekt/starteseite.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        h1
+        {
+            background-color: cornflowerblue;
+            width: 300px;
+            height: 70px;
+            padding: 70px;
+            margin 70px;
+
+        }background-color: black;
+
+    </style>
+</head>
+<body>
+
+
+<div id="bereich">
+<h1>Hygge </h1>
+    </div>
+
+
+
+
+</body>
