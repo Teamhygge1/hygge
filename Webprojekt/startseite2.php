00ccd309242aaf409c0c15113a2f8dc1e17e4f29
Koch <lk131@hdm-stuttgart.de>
2018-11-23 09:26:53 +0100

    Startseite

---
+++ Webprojekt/startseite2.php
@@ -0,0 +1,76 @@
+<?php include ("Startseite_style.css") ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+include
+
+</head>
+<div id="fullscreen_bg" class="fullscreen_bg"/>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-5 col-md-12 col-sm-8 col-xs-9 bhoechie-tab-container">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
+                <ul class="list-group">
+                    <a href="#" class="list-group-item active">
+                        <br/><br/><i class="glyphicon glyphicon-home"></i> Home<br/><br/>
+                    </a>
+                    <a href="#" class="list-group-item ">
+                        <br/><br/><i class="glyphicon glyphicon-envelope"></i> Nachrichten<br/><br/>
+                    </a>
+                    <a href="#" class="list-group-item ">
+                        <br/><br/><i class="glyphicon glyphicon-user"></i> Follower<br/><br/>
+                    </a>
+                    <a href="#" class="list-group-item">
+                        <br/><br/><i class="glyphicon glyphicon-cog"></i>Einstellungen<br/><br/>
+                    </a>
+                    </a>
+                    <a href="#" class="list-group-item">
+                        <br/><br/><i class="glyphicon glyphicon-cog"></i>Suche<br/><br/>
+                    </a>
+                </ul>
+            </div>
+            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
+                <!-- flight section -->
+                <div class="bhoechie-tab-content active">
+                    <center>
+                        <h1 class="glyphicon glyphicon-user" style="font-size:14em;color:#00001a"></h1>
+                        <h2 style="margin-top: 0;color:#00001a">Welcome</h2>
+                        <h3 style="margin-top: 0;color:#00001a">User HomePage</h3>
+                    </center>
+                </div>
+
+
+                <div class="bhoechie-tab-content">
+                    <center>
+                        <h1 class="glyphicon glyphicon-tasks" style="font-size:12em;color:#00001a"></h1>
+                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Schedule</a></h2>
+                        <h3 style="margin-top: 0;color:#00001a">Meine Follower</h3>
+                    </center>
+                </div>
+
+
+                <div class="bhoechie-tab-content">
+                    <center>
+                        <h1 class="glyphicon glyphicon-transfer" style="font-size:12em;color:#00001a"></h1>
+                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Trips</a></h2>
+                        <h3 style="margin-top: 0;color:#00001a">Meine Nachrichten </h3>
+                    </center>
+                </div>
+
+                <div class="bhoechie-tab-content">
+                    <center>
+                        <h1 class="glyphicon glyphicon-edit" style="font-size:12em;color:#00001a"></h1>
+                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Edit</a></h2>
+                        <h3 style="margin-top: 0;color:#00001a"><Einstellungen/h3>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
