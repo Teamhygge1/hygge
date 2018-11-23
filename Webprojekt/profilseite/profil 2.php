20447a153a8dde9d33de9119107acf8fe23cb6ea
jana- <jg119@hdm-stuttgart.de>
2018-11-23 08:41:40 +0100

    header 2 schöner

--- Webprojekt/profilseite/profil 2.php
+++ Webprojekt/profilseite/profil 2.php
@@ -1,27 +1,25 @@
 <?php
-include_once('../header 2.php');
-session_start();
+require ('../header2.php');
+require ('datenbank.php');
+
 $email = 'jg119@hdm-stuttgart.de';
 ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
 
 <div id="Profilseite">
     <div class="col-4">
 
 
-<div class="container">
+<div class="container container-content">
     <div class="row">
-        <div class="col-md-6 img">
-            <img src="rizky-subagja-1155257-unsplash.jpg"  alt="" class="img-rounded">
+        <div class="col-md-4 img">
+            <img src="rizky-subagja-1155257-unsplash.jpg" style="height: 400px; width:400px;" alt="" class="img-rounded">
         </div>
-         <div class="col-md-6 details" id="Benutzerinfo"
+
+         <div class="col-md-4"
             <h3>Infos Über mich:</h3>
             <?php
-            $statement = $pdo->prepare("SELECT 'E-Mail', 'Name', 'Kürzel', 'Studiengang' FROM 'Profil' WHERE $email='E-Mail'");
+            $statement = $db->prepare("SELECT 'E-Mail', 'Name', 'Kürzel', 'Studiengang' FROM 'Profil' WHERE $email='E-Mail'");
             $result = $statement->execute();
             while ($zeile = $result->fetchObject()) {
                 echo "<ul>$zeile</ul>";
@@ -30,10 +28,10 @@ $email = 'jg119@hdm-stuttgart.de';
             ?>
 
 
-            <div id="posts">
+            <div>
             <h3>Meine Posts:</h3>
             <?php
-            $statement = $pdo->prepare("SELECT 'E-Mail',  FROM 'Posts' WHERE $email='E-Mail'");
+            $statement = $db->prepare("SELECT 'E-Mail',  FROM 'Posts' WHERE $email='E-Mail'");
             $result = $statement->execute();
             while ($zeile = $result->fetchObject()) {
                 echo "<ul>$zeile</ul>";
