7536a0522fe5fd135b5f9bcc778c29dc3ebc2c27
jana- <jg119@hdm-stuttgart.de>
2018-12-19 20:27:28 +0100

    header 2 schöner

---
+++ Webprojekt/profilseite/profil2.php
@@ -0,0 +1,41 @@
+<?php
+require('../header2.php');
+require('../datenbank.php');
+
+$email = "jg119@hdm-stuttgart.de";
+$select_sql = $db->prepare("SELECT 'HdM Email' FROM Profil");
+$profil = $select_sql->execute();
+echo $profil;
+?>
+
+
+<div id="Profilseite">
+    <div class="col-4">
+
+
+        <div class="container container-content">
+            <div class="row">
+                <div class="col-md-4 img">
+                    <img src="IMG_5903.JPG" height="1000px">
+                </div>
+
+                <div class="col-md-4"
+                <h3>Infos Über mich:</h3>
+
+                <?php
+
+                echo $profil;
+                ?>      </div>
+            <div>
+                <h3>Meine Posts:</h3>
+
+                <?php
+                $statement = $db->prepare("SELECT 'Hdm Email' FROM 'postings' WHERE $email='Hdm Email'");
+                $result = $statement->execute();
+                ?>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
