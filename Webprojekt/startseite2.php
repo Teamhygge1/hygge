d21e2af372476f88a04d1e76d004ce496ef7a870
Luisa <44492107+Teamhygge1@users.noreply.github.com>
2019-01-05 16:09:36 +0100

    Add files via upload

--- Webprojekt/startseite2.php
+++ Webprojekt/startseite2.php
@@ -1,58 +1,70 @@
-<?php include("Startseite_style.css"); ?>
+<?php include("header2.php"); include_once ("datenbank.php"); ?>
+
+ <link rel="stylesheet" href="Starteite_style.css" >
 
 <!DOCTYPE html>
 <html>
 <head>
 
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
 
-<!------ Include the above in your HEAD tag ---------->
-include
+<!-- Include the above in your HEAD tag ---------->
+
 
 </head>
 
 <div id="fullscreen_bg" class="fullscreen_bg"/>
 <div class="container">
+    <div class="bhoechie-tab-content active">
+
+        <h2 style="margin-top: 0;color:#00001a">Willkommen zurück </h2>
+
+        <!-- eigenes Profil anzeigen? -->
+
+    </div>
     <div class="row">
         <div class="col-lg-5 col-md-12 col-sm-8 col-xs-9 bhoechie-tab-container">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 bhoechie-tab-menu">
+            <div class=" bhoeccol-md-3 col-md-3 col-md-3 col-md-3hie-tab-menu">
                 <ul class="list-group">
                     <a href="#" class="list-group-item active">
-                        <br/><br/><i class="glyphicon glyphicon-home"></i> Home<br/><br/>
-                    </a>
-                    <a href="#" class="list-group-item ">
-                        <br/><br/><i class="glyphicon glyphicon-envelope"></i> Nachrichten<br/><br/>
+                        <br/><br/><i class="glyphicon glyphicon-home"></i> Dein Profil <br/><br/>
 
-
-                    </a>
 
                     <a href="#" class="list-group-item ">
                         <br/><br/><i class="glyphicon glyphicon-user"></i> Follower<br/><br/>
 
 
-                    </a>
+                    <!--</a>
                     <a href="#" class="list-group-item">
-                        <br/><br/><i class="glyphicon glyphicon-search"></i>Suche<br/><br/>
+                        <br/><br/><i class="glyphicon glyphicon-search"></i>Suche<br/><br/> -->
 
-                    <a href="#" class="list-group-item">
-                        <br/><br/><i class="glyphicon glyphicon-cog"></i>Einstellungen<br/><br/>
+                   <!-- <a href="#" class="list-group-item">
+                        <br/><br/><i class="glyphicon glyphicon-cog"></i>Einstellungen<br/><br/>-->
+                        <select>
+                            <option value="p1"> Passwort</a></option>
+                            <option value="p2"> <a href="passwortvergessen.php.">Passwort vergessen</a></option>
+                            <option value="p3"> <a href="passwortzuruecksetzen.php">Passwort zurücksetzen</a></option>
+
+                        </select>
                     </a>
                 </ul>
             </div>
             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
                 <!-- flight section -->
-                <div class="bhoechie-tab-content active">
-                    <center>
-                        <h1 class="glyphicon glyphicon-user" style="font-size:14em;color:#00001a"></h1>
-                        <h2 style="margin-top: 0;color:#00001a">Welcome</h2>
-                        <h3 style="margin-top: 0;color:#00001a">User HomePage</h3>
-                    </center>
-                </div>
 
 
-                <div class="bhoechie-tab-content">
+<?php
+$statement = $pdo->prepare("SELECT * FROM 'postings'");
+$result = $statement->execute();
+while ($user = $statement->fetch()){
+    echo "<a>".$user["text"]."</a>";
+}
+
+?>
+
+               <!-- <div class="bhoechie-tab-content">
                     <center>
                         <h1 class="glyphicon glyphicon-tasks" style="font-size:12em;color:#00001a"></h1>
                         <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Schedule</a></h2>
@@ -79,4 +91,4 @@ include
             </div>
         </div>
     </div>
-</div>
+</div>  kommentar kommentar -->
