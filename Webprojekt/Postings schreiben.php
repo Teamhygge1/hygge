76d093941b89e656f7052251fb08a06919e020da
Luisa <44492107+Teamhygge1@users.noreply.github.com>
2019-01-12 18:27:23 +0100

    postings schreiben ohne logik

---
+++ Webprojekt/Postings schreiben.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Post schreiben</title>
+    <meta charset="UITF-8">
+<body>
+
+<form method="post">
+    <p><label>Dein Post:<br></label</p>
+
+    <textarea name="text" cols="50" rows="8"></textarea></label</p>
+    <input type="submit" value="OK">
+</form>
+
+<fieldset>
+<legend> Wie fühlst du dich heute?</legend>
+<select>
+     <option value="p1"> <a> hyggelig</a></option>
+    <option value="p2"> <a> glücklich </a></option>
+    <option value="p2"> <a> glücklich </a></option>
+    <option value="p2"> <a> glücklich </a></option>
+    <option value="p2"> <a> glücklich </a></option>
+    <option value="p2"> <a> glücklich </a></option>
+    <option value="p2"> <a> glücklich </a></option>
+
+</select>
+</fieldset>
+</body>
+</head>
+</html>
