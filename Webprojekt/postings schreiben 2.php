b4b02caab36aa458cda07b861f548b0c4204bf80
Luisa <44492107+Teamhygge1@users.noreply.github.com>
2019-01-24 16:52:41 +0100

    Add files via upload

--- Webprojekt/postings schreiben 2.php
+++ Webprojekt/postings schreiben 2.php
@@ -2,8 +2,8 @@
 include 'datenbank.php';
 
 session_start();
-$Body = $_POST["Body"]; // get oder post
-// noch userid irgendwie abfragen und einlesen
+$Body = $_POST["Body"];// get oder post
+$User = $_POST ["User"]; // noch user irgendwie abfragen und einlesen
 //$email = $_SESSION["email"];
 //Posts in Datenbank schreiben
 
@@ -18,7 +18,7 @@ $statement->bindParam(':email', $_POST["email"]);
 $statement->bindParam(':Body', $_POST["Body"]);
 $statement->bindParam(':User', $_POST["User"]);
 ?>
-
+<link rel="stylesheet" href="postingsschreiben.css" >
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,7 +30,7 @@ $statement->bindParam(':User', $_POST["User"]);
 <form method="post" action ="<?php echo $_SERVER[`PHP_SEL`];?>">
     <p><label>Dein Post:<br></label</p>
 
-    <textarea name="Body" cols="50" rows="8"></textarea></label</p>
+    <textarea name="Body" ></textarea> </label</p>
     <input type="submit" value="Posten">
 </form>
 
