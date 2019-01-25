fd91db5196b9ce9f6f4e49f2a7af1c4f0d1527c0
Koch <lk131@hdm-stuttgart.de>
2019-01-25 18:43:03 +0100

    neu

--- Webprojekt/startseite22.php
+++ Webprojekt/startseite22.php
@@ -1,11 +1,11 @@
 <?php
 include("header2.php");
 include_once ("datenbank.php");
-include ("postings schreiben 2.php");
+include ("postings_schreiben_2.php");
 session_start();
 ?>
 
- <link rel="stylesheet" href="Starteite_style.css" >
+<link rel="stylesheet" href="Starteite_style.css" >
 
 <!DOCTYPE html>
 <html>
@@ -15,7 +15,7 @@ session_start();
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
 
-<!-- Include the above in your HEAD tag ---------->
+    <!-- Include the above in your HEAD tag ---------->
 
 
 </head>
@@ -55,65 +55,61 @@ session_start();
                             <select>
 
                                 < <option value="p2"> <a href="passwortvergessen.php.">Passwort vergessen</a></option>
-                            <option value="p3"> <a href="passwortzuruecksetzen.php">Passwort zurücksetzen</a></option>
+                                <option value="p3"> <a href="passwortzuruecksetzen.php">Passwort zurücksetzen</a></option>
 
-                        </select>
-                    </a>
+                            </select>
+                        </a>
                 </ul>
             </div>
             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9 bhoechie-tab">
                 <!-- flight section -->
 
                 <form method ="post">
-                <fieldset>
-                    <legend> Hygellige Beiträge</legend>
-                    <div class="w3-container">
-                        <p>
+                    <fieldset>
+                        <legend> Hygellige Beiträge</legend>
+                        <div class="w3-container">
+                            <p>
 
-                    <?php
-                    $statement = $pdo->prepare ( "SELECT * FROM `Posts` order by created_at DESC");
-                    // $pdo = new PDO('mysql::host=mars.iuk.hdm-stuttgart.de;dbname=u-as327', 'as327', 'LahMaedae1');
-$sql = "SELECT * FROM Posts";
-                   foreach ($pdo->query($sql) as $row) {
-                       $email= $row['email'];
-                        echo "<a href='./profilseite/profilvonaußen.php?andere=$email'>".$email."</a> schrieb: <br/>
+                                <?php
+                                $statement = $pdo->prepare ( "SELECT * FROM `Posts` order by created_at DESC");
+                                // $pdo = new PDO('mysql::host=mars.iuk.hdm-stuttgart.de;dbname=u-as327', 'as327', 'LahMaedae1');
+                                $sql = "SELECT * FROM Posts";
+                                foreach ($pdo->query($sql) as $row) {
+                                    $email= $row['email'];
+                                    echo "<a href='./profilseite/profilvonaußen.php?andere=$email'>".$email."</a> schrieb: <br/>
                         ".$row['Body']."<br/>";
-                        echo "geschrieben am: " .$row['created_at']."<br /> <br/>";
-
-                    }
-                    ?>
-                        </p></div>
-</fieldset>
+                                    echo "geschrieben am: " .$row['created_at']."<br /> <br/>";
+                                }
+                                ?>
+                            </p></div>
+                    </fieldset>
                 </form>
 
 
 
 
-               <!-- <div class="bhoechie-tab-content">
-                    <center>
-                        <h1 class="glyphicon glyphicon-tasks" style="font-size:12em;color:#00001a"></h1>
-                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Schedule</a></h2>
-                        <h3 style="margin-top: 0;color:#00001a">Meine Follower</h3>
-                    </center>
-                </div>
-
-
-                <div class="bhoechie-tab-content">
-                    <center>
-                        <h1 class="glyphicon glyphicon-transfer" style="font-size:12em;color:#00001a"></h1>
-                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Trips</a></h2>
-                        <h3 style="margin-top: 0;color:#00001a">Meine Nachrichten </h3>
-                    </center>
-                </div>
-
-                <div class="bhoechie-tab-content">
-                    <center>
-                        <h1 class="glyphicon glyphicon-edit" style="font-size:12em;color:#00001a"></h1>
-                        <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Edit</a></h2>
-                        <h3 style="margin-top: 0;color:#00001a"><Einstellungen/h3>
-                    </center>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>  kommentar kommentar -->
+                <!-- <div class="bhoechie-tab-content">
+                     <center>
+                         <h1 class="glyphicon glyphicon-tasks" style="font-size:12em;color:#00001a"></h1>
+                         <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Schedule</a></h2>
+                         <h3 style="margin-top: 0;color:#00001a">Meine Follower</h3>
+                     </center>
+                 </div>
+                 <div class="bhoechie-tab-content">
+                     <center>
+                         <h1 class="glyphicon glyphicon-transfer" style="font-size:12em;color:#00001a"></h1>
+                         <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Trips</a></h2>
+                         <h3 style="margin-top: 0;color:#00001a">Meine Nachrichten </h3>
+                     </center>
+                 </div>
+                 <div class="bhoechie-tab-content">
+                     <center>
+                         <h1 class="glyphicon glyphicon-edit" style="font-size:12em;color:#00001a"></h1>
+                         <h2 style="margin-top: 0;color:#00001a"><a href="" class="btn btn-sm btn-primary btn-block" role="button">Edit</a></h2>
+                         <h3 style="margin-top: 0;color:#00001a"><Einstellungen/h3>
+                     </center>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>  kommentar kommentar -->
