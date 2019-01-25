23afc8cc0e18966fb84c942553fc26d3809460a0
Koch <lk131@hdm-stuttgart.de>
2019-01-25 17:11:22 +0100

    neu

---
+++ Webprojekt/follow.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once "datenbank.php";
+
+session_start();
+
+$email = $_SESSION["email"];
+$andere= $_GET ["andere" ];
+//$kuerzel = $_SESSION["kuerzel"]; #help
+
+// In Tabelle Abonnenten wird das "Follower-Verhältnis" vermerkt.
+$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+$sql = "INSERT INTO following (email, following) VALUES (?, ?)";
+
+if ($statement = $pdo->prepare($sql)) {
+    $statement->execute(array("$email", "$andere"));
+
+};
+
+/* Alle Posts des neuen Freundes werden als gelesen in der Tabelle Notification markiert.
+$stmt = $pdo->prepare("UPDATE posts SET $email ='read' WHERE post= ANY(SELECT id FROM posts WHERE email=:andere)");
+$stmt->execute(array(":email"=>"$andere")); */
+
+header("Location: profil_anderer.php?profilname=$email");
+?>
