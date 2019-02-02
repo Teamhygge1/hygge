a534a739d841ba132835315cb1ff3f25e0e79056
annischaub <as327@hdm-stuttgart.de>
2019-02-02 01:33:04 +0100

    Kommentare

--- Webprojekt/follow.php
+++ Webprojekt/follow.php
@@ -7,21 +7,21 @@ session_start();
 $email = $_SESSION["email"];
 $andere= $_GET ["andere"];
 
-// In Tabelle Abonnenten wird das "Follower-Verhältnis" vermerkt.
-//$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
-$sql = "INSERT INTO following (email, following) VALUES (?, ?)";
+$sql = "INSERT INTO following (email, following) VALUES (?, ?)"; //SQL Befehl-> Einfügen von Email und der E-Mail des anderen in die Datenbank
 
 if ($statement = $pdo->prepare($sql)) {
     $statement->execute(array("$email", "$andere"));
 
 };
-
-/* Alle Posts des neuen Freundes werden als gelesen in der Tabelle Notification markiert.
-$stmt = $pdo->prepare("UPDATE posts SET $email ='read' WHERE post= ANY(SELECT id FROM posts WHERE email=:andere)");
-$stmt->execute(array(":email"=>"$andere")); */
-
-// header("Location: profilvonaußen.php?profilname=$email");
 ?>
+
+
+
+
+
+
+
+
 
 
 
